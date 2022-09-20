a1303416d476929a0afaabd33aeb810a4a4d78d1
sltsheryl <sltsheryl@gmail.com>
2022-09-20 16:20:36 +0800

    Add dependencies

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import sqlite3
 from datetime import date
 from flask_session import Session
 
-
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 app.static_folder = 'static'
 
+if __name__ == "__main__":
+  app.run()
 
 def get_db_connection():
     conn = sqlite3.connect('database.db', check_same_thread=False)
